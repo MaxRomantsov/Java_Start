30fb9b47147f0ea581ecc69dbaeb898f38cee1c6
MaxRomantsov <Roma_ua72@student.itstep.org>
2024-03-30 03:49:47 +0200

    Edit and delete categories

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,9 @@ public class Main {
 
     public static void main(String[] args) {
         //insertCategory();
-        showCategories();
+        //showCategories();
+        //editCategory();
+        //deleteCategory();
     }
 
 
@@ -45,6 +47,59 @@ public class Main {
 
             for(var item : list) {
                 System.out.println(item);
+            }
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void editCategory() {
+        Scanner scanner = new Scanner(System.in);
+        Calendar calendar = Calendar.getInstance();
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+
+            System.out.println("Введіть ID категорії для редагування:");
+            int id = scanner.nextInt();
+
+            Category category = session.get(Category.class, id);
+
+            if (category != null) {
+                System.out.println("Вкажіть нову назву:");
+                String newName = scanner.nextLine();
+                scanner.nextLine();
+                category.setName(newName);
+                System.out.println("Вкажіть нове фото:");
+                String newImage = scanner.nextLine();
+                category.setImage(newImage);
+                session.update(category);
+                System.out.println("Категорію оновлено успішно!");
+            } else {
+                System.out.println("Категорія не знайдена!");
+            }
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void deleteCategory() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+
+            System.out.println("Введіть ID категорії для видалення:");
+            int id = new Scanner(System.in).nextInt();
+
+            Category category = session.get(Category.class, id);
+
+            if (category != null) {
+                session.delete(category);
+                System.out.println("Категорію видалено успішно!");
+            } else {
+                System.out.println("Категорія не знайдена!");
             }
 
             session.getTransaction().commit();
